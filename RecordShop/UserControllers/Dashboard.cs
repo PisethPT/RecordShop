ed3f6f66d99d307298bc70afbd3ca34efb37c735
YOS PISETH <201002@student.sru.edu.kh>
2024-10-27 21:49:31 +0700

    remove using not used

--- RecordShop/UserControllers/Dashboard.cs
+++ RecordShop/UserControllers/Dashboard.cs
@@ -1,14 +1,6 @@
 ﻿using RecordShop.Data;
 using RecordShop.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace RecordShop.UserControllers
 {
